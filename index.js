73ada8f657b8d8f055fb7e903e4f142f10a860b5
ramiro <ramiro@ramiros-MacBook-Pro.local>
2017-08-19 18:02:09 -0400

    routes finished

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var Twitter = require('twitter');
 var models = require('./models');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json({ limit: '10mb' }));
 var client = new Twitter({
   consumer_key: 'Qml8euMh6wNf68yM6tzY4l6qq',
   consumer_secret: 'AMIRz4GOMK7eLr3EgT7Omxf0Xbkole0iGHozgLctQ3c8ReNPVb',
@@ -10,8 +12,9 @@ var client = new Twitter({
   access_token_secret: 'FVcXZD1tabksftgYxLnSzy60pbBkvuAjYw7clbxFVEcKt'
 });
 let myTweets = []
+// get tweets from twitter
 app.get('/', function(req, res, next) {
-  client.get('search/tweets', {q: 'bolivia'}, function(error, tweets, response) {
+  client.get('search/tweets', {q: req.query.q}, function(error, tweets, response) {
     for(let element of tweets.statuses){
         let myTweet = {}
         myTweet.tweet_text = element.text
@@ -22,17 +25,37 @@ app.get('/', function(req, res, next) {
         myTweet.location = element.user.location
         myTweets.push(myTweet)
     }
-    //save data
-    models.tweet.bulkCreate(myTweets)
-    .then(result => {
-        console.log('tweets saved!')
-    })
-    .catch(error => {
-        console.log('error XXXXXXXXXXXXXXXXXXXXXX', error)
-    })
-
     res.send(myTweets)
   });
 });
+// save tweets
+app.post('/save', function(req, res, next) {
+    models.tweet.bulkCreate(req.body)
+    .then(result => {
+        console.log('tweets saved!')
+        res.send({
+        finalizado: true,
+        message: 'todo ok'
+    })
+    })
+    .catch(error => {
+        console.log('error XXXXXXXXXXXXXXXXXXXXXX', error)
+        res.send({
+            finalizado: false,
+            message: error
+        })
+    })
+});
+// get tweets from db
+app.get('/list', function(req, res, next) {
+    models.tweet.findAll()
+    .then(result => {
+        res.send(result)
+    })
+    .catch(error => {
+        res.send(error)
+    })
+});
+
 
 app.listen(3006);
