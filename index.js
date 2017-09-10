d8973fb674e2b7784f816336a3b7c5dcc558374c
ramiro <ramiro@ramiros-MacBook-Pro.local>
2017-09-10 12:35:24 -0400

    hasta aqui funciona ok. solo falta exportar a xlsx

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Twitter = require('twitter');
 var models = require('./models');
 var bodyParser = require('body-parser');
 var cors = require('cors')
+var moment = require('moment')
 
 var app = express();
 
@@ -17,15 +18,15 @@ var client = new Twitter({
 // get tweets from twitter
 app.get('/', function(req, res, next) {
   client.get('search/tweets', {q: req.query.q}, function(error, tweets, response) {
-   let myTweets = [] 
+   let myTweets = []
    for(let element of tweets.statuses){
         let myTweet = {}
         myTweet.code = element.id_str
         myTweet.text = element.text
         myTweet.username = element.user.screen_name
         myTweet.name = element.user.name
-        myTweet.date = element.user.created_at
-        myTweet.hour = '10:10'
+        myTweet.date = moment(element.created_at).format('YYYYMMDD')
+        myTweet.hour = moment(element.created_at).format('hh:mm:ss')
         myTweet.likes = 0 // find out how to obtain
         myTweet.rts = 0 // find out how to obtain
         myTweet.hashtags = element.entities.hashtags.map(x => x.text)
@@ -57,6 +58,23 @@ app.post('/save', function(req, res, next) {
 // get tweets from db
 app.get('/list', function(req, res, next) {
     models.tweet.findAll()
+    .then(result => {
+        res.send(result)
+    })
+    .catch(error => {
+        res.send(error)
+    })
+});
+
+// search tweets on db
+app.get('/search', function(req, res, next) {
+    models.tweet.findAll({
+        where: {
+            text: {
+                $like: `%${req.query.q}%` 
+            }
+        }
+    })
     .then(result => {
         res.send(result)
     })
